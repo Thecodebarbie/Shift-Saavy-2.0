1d27551105fd0dee005386066fa8cb1d2c99a8b9
Rasmi Roy <rasmiroy@yahoo.com>
2024-05-23 20:26:25 -0400

    created the graphQL queries and mutations structure and resolvers for queries and mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,25 +1,48 @@
-const { User } = require('../models');
+const { User, Schedule } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
+      console.log('User in context:', context.user);
       if (context.user) {
-        return User.findOne({ _id: context.user._id });
+        const user = await User.findById(context.user._id);
+        if (!user) {
+          throw new AuthenticationError('User not found');
+        }
+        return user;
       }
-      throw AuthenticationError;
-    }
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      try {
+        const users = await User.find({});
+        return users;
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        throw new Error('Failed to fetch users');
+      }
+    },
+    schedules: async () => {
+      return Schedule.find({});
+    },
+    schedule: async (parent, { id }) => {
+      return Schedule.findById(id);
+    },
+    userSchedules: async (parent, { userId }) => {
+      return Schedule.find({ userId });
+    },
   },
 
   Mutation: {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw AuthenticationError;
+        throw new AuthenticationError('Incorrect credentials');
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
-        throw AuthenticationError;
+        throw new AuthenticationError('Incorrect credentials');
       }
       const token = signToken(user);
       return { token, user };
@@ -29,7 +52,21 @@ const resolvers = {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
-    }
+    },
+
+    addSchedule: async (parent, args, context) => {
+      if (context.user) {
+        return Schedule.create({ ...args, userId: context.user._id });
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+
+    removeSchedule: async (parent, { id }, context) => {
+      if (context.user) {
+        return Schedule.findByIdAndDelete(id);
+      }
+      throw new AuthenticationError('Not logged in');
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,23 +3,35 @@ const typeDefs = `
     _id: ID
     username: String!
     email: String!
-    password: String!
+    managerId: Int
   }
 
+  type Schedule {
+    _id: ID
+    userId: ID
+    date: String
+    startTime: String
+    endTime: String
+  }
 
   type Auth {
     token: ID!
     user: User
   }
 
-
   type Query {
     me: User
+    users: [User]
+    schedules: [Schedule]
+    schedule(id:ID!) : Schedule
+    userSchedules(userId: ID!): [Schedule]
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
+    addSchedule(userId: ID!, date: String!, startTime: String!, endTime: String!): Schedule
+    removeSchedule(id: ID!): Schedule
   }
 `;
 
