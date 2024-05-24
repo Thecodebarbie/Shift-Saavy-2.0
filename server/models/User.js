dc1236f800262f2e6f48fb3a17f98fffe0b714f8
Rasmi Roy <rasmiroy@yahoo.com>
2024-05-23 20:17:55 -0400

    created the user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+
 const userSchema = new Schema(
   {
     username: {
@@ -17,6 +18,9 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true,
+    },
+    managerId: {
+      type: Number,
     }
   }
 );
