b8c37d3b6802534fe4e2e3071da8195b4d4583a4
Rasmi Roy <152426560+rasdevworld@users.noreply.github.com>
2024-05-26 06:11:57 -0400

    Models/rasmi (#2)
    * saved
    
    * setup server configuration and added the date format file in the utils folder
    
    * created the user model
    
    * created the schedule model and imported the user and schedule models

---
+++ server/models/Schedule.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose')
+//const dateFormat = require('../utils/dateFormat')
+
+
+const scheduleSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      startTime: {
+        type: String,
+        required: true,
+      },
+      endTime: {
+        type: String,
+        required: true,
+      },
+})
+
+const Schedule = model('Schedule', scheduleSchema);
+
+module.exports =Schedule;
--- server/models/User.js
+++ server/models/User.js
@@ -18,6 +18,9 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true,
+    },
+    managerId: {
+      type: Number,
     }
   }
 );
