9753154778aad3d7b7741e8cb68005858e7b3dcd
Thecodebarbie <158063911+Thecodebarbie@users.noreply.github.com>
2024-05-28 20:27:52 -0400

    Pages (#7)
    * created jsx files for app pages.
    
    * import landing code from ShiftSaavy beta
    
    * added path&element.
    
    * import css.
    
    * import code for auth, nav pages. Added images to public folder.  Added path for Authenticate Page.
    
    * created Login page and path
    
    * Created  page and path for Signup.
    
    * created dashboard layout.
    
    * moved css files from public to src. import css files.
    
    * naming convention updates for landing,login, signup. also made changes to Dashboard directions.
    
    * "saved"
    
    * saved
    
    ---------
    
    Co-authored-by: THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App.jsx'
+import Landing from './pages/Landing.jsx'
+import Authenticate from './pages/Authenticate.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+
 
 
 const router = createBrowserRouter([
@@ -11,9 +17,28 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
-      
+      {
+        path: '/',
+        element: <Landing />
+      },
+      {
+        path:'/authenticate',
+        element: <Authenticate />
+      },
+      {
+        path:'/login',
+        element: <Login />
+      },
+      {
+        path:'/signup',
+        element: <Signup />
+      },
+      {
+        path:'/dashboard',
+        element: <Dashboard />
+      }
     ]
-  }
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
