be9176822ffd2bebc374650a00a9e64d7c5ca5a7
Rasmi Roy <152426560+rasdevworld@users.noreply.github.com>
2024-05-28 20:15:09 -0400

    Seeders/rasmi (#6)
    * saved
    
    * setup server configuration and added the date format file in the utils folder
    
    * created the user model
    
    * created the schedule model and imported the user and schedule models
    
    * created the graphQL queries and mutations structure and resolvers for queries and mutations
    
    * created user seeds
    
    * created the schedule seeds
    
    * created functionality to execute user and schedule seeds
    
    * added the seed to the script
    
    * debugged schedule rendering on graphQL and solved
    
    * resolved the schedule creation issue
    
    * resolved the userSchedules resolver
    
    * saved
    
    * updated the typeDefs file
    
    * created the calloff model
    
    * created the schemas for the calloff feature
    
    * created the mutation for calloff feature
    
    * created the query and mutation for calloff features
    
    * updated the README file
    
    * created the mutations for login and signup

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,16 +1,23 @@
 import {gql} from '@apollo/client'
 
-export const ADD_USER = gql`
-mutation AddUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-    }
-  }
-`
 export const LOGIN = gql`
 mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      email
+      username
+      managerId
     }
   }
+}
 `
+export const ADD_USER = gql`
+mutation AddUser($username: String!, $email: String!, $password: String!) {
+  addUser(username: $username, email: $email, password: $password) {
+    token
+  }
+}
+`
+
