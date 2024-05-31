144694129a8a6d4b7ba3a2a7d581a1d39779b7e6
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-30 22:50:05 -0400

    calloff . . .

---
+++ client/src/components/CalloffForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function CalloffForm(props) {
+    return (
+<>
+
+
+<section class="container">
+        <input type="checkbox" id="signup_toggle" hidden/>
+        <article class="form"/>
+            <form id="form-signup" class="form_front"/>
+                <h2 class="form_details">Call Off Request</h2>
+                <input id="firstName" type="text" class="input" placeholder="First Name"/>
+                <input id="lastName" type="text" class="input" placeholder="Last Name"/>
+                <input id="employee-id" type="text" class="input" placeholder="Employee ID"/>
+                <input id="shift-date" type="password" class="input" placeholder="Shift Date"/>
+                <input id="shift-start" type="text" class="input" placeholder="Shift Start Time"/>
+                <input id="shift-end" type="text" class="input" placeholder="Shift End Time"/>
+                <button id="calloff-btn" type="submit" class="btn">Submit Call Off</button>
+                <p class="switch">Want to swap shifts? <span class="signup_tog">Shift Swap</span></p>
+    </section>
+
+    <section id="success-section" class="container">
+        <input type="checkbox" id="calloff_toggle_success" hidden/>
+        <article class="form">
+            <form id="form-success" class="form_front">
+                <h2 class="form_details">You have successfully called off</h2>
+            </form>
+        </article>
+    </section>
+</>
+    );
+}
+
+export default CalloffForm;
