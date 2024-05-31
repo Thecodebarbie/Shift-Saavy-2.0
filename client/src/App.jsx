fc74bda236a35fc3a1536b6c42a2ee393c7e7d48
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-31 17:27:02 -0400

    fixed calloff and calloff css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200">
       <link rel="stylesheet" href="./css/schedule.css">
-
+      <link rel="stylesheet" href="/css/callOff.css">
       <link id="dashboard" rel="stylesheet" href="/css/dashboard.css">
   `
     }
