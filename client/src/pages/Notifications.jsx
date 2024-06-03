c0f422a22d024be7716b26fe6d7413e76eab677f
Rasmi Roy <152426560+rasdevworld@users.noreply.github.com>
2024-06-02 21:47:24 -0400

    Updates/rasmi (#18)
    * calloff routing
    
    * updated the dashboardtable and signup functionality
    
    * created mutation for calloff
    
    * implemented the calloff functionality on clientside
    
    * added some functionality to render schedules by status and calloff submit
    
    * dayjs import
    
    * implemented the next shift date on dashboard
    
    * removed the duplicate entry
    
    * saved
    
    * implemented the welcome message on dashboard
    
    * implemented the functionality to pull the full name for schedules
    
    * features for notification
    
    * implemented fetch feature for calloff
    
    * calloff display based on the data
    
    * created the notification feature for calloff
    
    * calloff feature
    
    * calloff feature
    
    * calloff feature
    
    * saved
    
    * implemented the notification delete
    
    ---------
    
    Co-authored-by: Thecodebarbie <158063911+Thecodebarbie@users.noreply.github.com>

--- client/src/pages/Notifications.jsx
+++ client/src/pages/Notifications.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useMutation, useQuery } from '@apollo/client';
-import { REMOVE_CALLOFF } from '../utils/mutations';
+import { useQuery } from '@apollo/client';
 import { QUERY_ME, QUERY_USER_CALLOFFS } from '../utils/queries';
 import NotificationList from '../components/NotificationList';
 import Sidebar from '../components/Sidebar';
@@ -10,6 +9,7 @@ function Notifications() {
   const { loading, error, data, refetch } = useQuery(QUERY_USER_CALLOFFS, {
     variables: { userId: meData?.me?._id },
   });
+
   const [callofflist, setCalloffList] = useState([]);
 
   useEffect(() => {
@@ -17,19 +17,6 @@ function Notifications() {
       setCalloffList(data.userCalloffs);
     }
   }, [data]);
-
-  const [deleteCalloff] = useMutation(REMOVE_CALLOFF);
-
-  const handleDeleteCalloff = async (calloffId) => {
-    try {
-      await deleteCalloff({ variables: { removeCalloffId: calloffId } });
-      console.log("Successfully removed the calloff with ID:", calloffId);
-      // Refetch the user calloff query to update the list after deletion
-      refetch();
-    } catch (error) {
-      console.error('Failed to delete calloff:', error);
-    }
-  };
 
   if (loading || meLoading) return <p>Loading...</p>;
   if (error || meError) return <p>Error: {error ? error.message : meError.message}</p>;
@@ -41,7 +28,7 @@ function Notifications() {
           <Sidebar />
         </div>
         <div className='col-9'>
-          <NotificationList callofflist={callofflist} onUpdateCalloffList={handleDeleteCalloff} />
+          <NotificationList callofflist={callofflist} setCalloffList={setCalloffList} />
         </div>
       </div>
     </>
