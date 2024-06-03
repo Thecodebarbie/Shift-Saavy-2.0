0bbeefe1328cbbf405cb7b5227e3fd7a02ee625b
Rasmi Roy <152426560+rasdevworld@users.noreply.github.com>
2024-06-02 22:16:41 -0400

    Updates/rasmi (#19)
    * calloff routing
    
    * updated the dashboardtable and signup functionality
    
    * created mutation for calloff
    
    * implemented the calloff functionality on clientside
    
    * added some functionality to render schedules by status and calloff submit
    
    * dayjs import
    
    * implemented the next shift date on dashboard
    
    * removed the duplicate entry
    
    * saved
    
    * implemented the welcome message on dashboard
    
    * implemented the functionality to pull the full name for schedules
    
    * features for notification
    
    * implemented fetch feature for calloff
    
    * calloff display based on the data
    
    * created the notification feature for calloff
    
    * calloff feature
    
    * calloff feature
    
    * calloff feature
    
    * saved
    
    * implemented the notification delete
    
    * saved changes on main.jsx
    
    * saved
    
    ---------
    
    Co-authored-by: Thecodebarbie <158063911+Thecodebarbie@users.noreply.github.com>

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 import App from './App.jsx'
 import Landing from './pages/Landing.jsx'
 import Login from './pages/Login.jsx'
@@ -10,7 +9,6 @@ import Dashboard from './pages/Dashboard.jsx'
 import CallOff from './pages/CallOff.jsx'
 import Schedule from './pages/Schedule.jsx'
 import Notification from './pages/Notifications.jsx'
-
 
 const router = createBrowserRouter([
   {
